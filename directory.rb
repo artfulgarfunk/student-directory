2e09ab98c7b7e61b252c0fbd2dcc767ae1a6c0ab
Jack <92jackhenderson@gmail.com>
2016-12-06 15:03:00 +0000

    with indexed students

--- directory.rb
+++ directory.rb
@@ -1,21 +1,4 @@
 
-students = []
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print(students)
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -34,8 +17,25 @@ def input_students
   # return the array of students
   students
 end
-student_count = students.count
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+    students.each_with_index { |student, index|
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  }
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+
 students = input_students
+student_count = students.count
 print_header
 print(students)
 print_footer(students)
