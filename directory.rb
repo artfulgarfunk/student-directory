f0d9d783f645e72e48519ae6b63e7acecf080df8
Jack <92jackhenderson@gmail.com>
2016-12-08 15:36:20 +0000

    load and save file

--- directory.rb
+++ directory.rb
@@ -49,6 +49,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -66,6 +67,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -107,6 +110,15 @@ def print_by_cohort
     end
   }
   puts cohort_sort
+end
+
+def load_students
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',') # setting the variables name and cohort equal to the two sides of a line split by ','
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def print_footer
