d58bd7f8bb26873402d9ae50ce3c011c8af89fc9
Jack <92jackhenderson@gmail.com>
2016-12-08 14:45:37 +0000

    student variable available to all program

--- directory.rb
+++ directory.rb
@@ -1,9 +1,10 @@
+@students = []
+
 def input_students
-  students = []
   #getting all default values together
   defaults = {
     cohort: "Sometime soon!",
-    age: "Between 0 and 100, probably",
+    age: "Between 0 and 100, judging by his face",
     birthplace: "Earth (probably)",
     hobby: "TV I guess",
   }
@@ -37,19 +38,52 @@ def input_students
     hobby = STDIN.gets.gsub(/\n/,"")
       if hobby.empty? then hobby = defaults[:hobby] end
 
-    students << {name: name,cohort: cohort.to_sym,age: age,birthplace: birthplace, hobby: hobby}
-    puts "Now we have #{students.count} students".center(50,'--')
+    @students << {name: name,cohort: cohort.to_sym,age: age,birthplace: birthplace, hobby: hobby}
+    puts "Now we have #{@students.count} students".center(50,'--')
     name = gets.gsub(/\n/,"")
   end
-  students
+  @students
 end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def interactive_menu
+  loop do
+  # 1. print the menu and ask the user what to do
+  print_menu
+  # 2. read the input and save it into a variable
+  selection = gets.chomp
+  # 3. do what the user has asked, computer
+  case selection
+    when "1"
+      @students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit #get out of the program!
+    else
+      puts "That doesn't even make sense human"
+    end
+  end
+end
+  # 4. repeat from step 1
 
 def print_header
   puts "The students of Villains Academy"
 end
 
-def print(students)
-    students.each_with_index { |student, index|
+def print
+    @students.each_with_index { |student, index|
       puts "#{index+1}. #{student[:name]}".center(50,'--')
       puts "Cohort: #{student[:cohort]}".center(50,'--')
       puts "Age: #{student[:age]}".center(50,'--')
@@ -58,12 +92,12 @@ def print(students)
   }
 end
 
-def print_by_cohort(students)
+def print_by_cohort
   cohort_sort = []
   puts "Which cohort?"
   input_cohort = gets.gsub(/\n/,"")
   input_cohort = input_cohort.to_sym
-  students.each { |student|
+  @students.each { |student|
     if student[:cohort] == input_cohort
       cohort_sort << student
     else
@@ -72,24 +106,24 @@ def print_by_cohort(students)
   puts cohort_sort
 end
 
-def print_footer(students)
-  if students.count >= 2
+def print_footer
+  if @students.count >= 2
     maybe_plural = "students"
   else
     maybe_plural = "student"
   end
-  footer = "Overall we have #{students.count} great #{maybe_plural}".center(50,'--')
+  footer = "Overall we have #{@students.count} great #{maybe_plural}".center(50,'--')
   puts footer
 end
 
-def select_first_letter(students)
+def select_first_letter
   puts "do you want to sort by letter?"..center(50,'--')
   input=gets.gsub(/\n/,"")
   if input == "yes"
   sorted_students = []
       puts "What letter?"
       letter = gets.gsub(/\n/,"")
-      students.each { |x|
+      @students.each { |x|
         if x[:name][0] == letter
         sorted_students << x
         else
@@ -100,9 +134,9 @@ def select_first_letter(students)
   end
 end
 
-def select_less_than(students)
+def select_less_than
   less_twelve = []
-  students.each { |x|
+  @students.each { |x|
       if x[:name].length <= 11
       less_twelve << x
       else
@@ -111,11 +145,10 @@ def select_less_than(students)
     puts "These are less than twelve...", less_twelve
 end
 
-students = input_students
-student_count = students.count
+interactive_menu
 print_header
-print(students)
+print
 #print_by_cohort(students)
-print_footer(students)
+print_footer
 #select_first_letter(students)
 #select_less_than(students)
