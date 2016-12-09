2bf25ed919a122b68fa903102c3a225ee911bb57
Jack <92jackhenderson@gmail.com>
2016-12-09 12:38:53 +0000

    tidy source code

--- source.rb
+++ source.rb
@@ -1,10 +1,6 @@
 # So meta...
 # META EVEN THIS ACRONYM!!
 
-puts __FILE__
-
-File.read(__FILE__)
-
 file = File.open(__FILE__, mode = 'r') { |file|
     file.readlines.each do |line|
       puts line
